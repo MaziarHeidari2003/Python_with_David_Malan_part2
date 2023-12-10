de0b402c5f046aae9d41c1f880aa0b152cda9dcf
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-10 06:06:15 -0800

    my commit

---
+++ say.py
@@ -0,0 +1,5 @@
+import sys
+import cowsay
+
+if len(sys.argv) == 2 :
+  cowsay.__builtins__
--- sys_library.py
+++ sys_library.py
@@ -1,8 +1,19 @@
 import sys
+"""
+if len(sys.argv) < 2 :
+  #print("Few arguements")
+    sys.exit("Few arguements")
 
-if len(sys.argv) > 2 :
-  sys.exit("Too many arguements")
-elif len(sys.argv) < 2 :
-  sys.exit("Too few arguements")
+elif len(sys.argv) > 2 :
+  #print("Too many arguements")
+    sys.exit("Too many arguements")
 
-print("hello , my name is", sys.argv[1])
+else :
+  print("hello, your name is", sys.argv[1])    
+  """
+
+if len(sys.argv) < 2 :
+    sys.exit("Few arguements")
+
+for arg in sys.argv[1:]:
+    print("hello my name is", arg)    
