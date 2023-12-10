9ad9350dfe832ecc277a2d2433f0073ce868361b
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-10 10:38:44 -0800

    up to writing my own packages

--- itunes.py
+++ itunes.py
@@ -5,9 +5,10 @@ import json
 if len(sys.argv) != 2 :
   sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" +sys.argv[1])
-#print(json.dumps(response.json(), indent=2 ))
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+#print(json.dumps(response.json(), indent =2))
 
 o = response.json()
-for result in o["results"]:
-  print(result["trackName"])  
+for result in o["results"] :
+  print(result["trackName"])
