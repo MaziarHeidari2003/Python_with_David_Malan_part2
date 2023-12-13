fe94a4ee8b31a037bbf22a395d96b635d888d920
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-12 23:43:32 -0800

    adieu

---
+++ pset4_adieu.py
@@ -0,0 +1,14 @@
+import inflect 
+p = inflect.engine()
+
+names = []
+while True:
+  try:
+    name = input('Name: ')
+    names.append(name)
+  except EOFError:  
+    print()
+    break
+
+output = p.join(names)
+print('Adieu,adieu to',output)  
